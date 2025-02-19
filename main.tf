383714bbcaf74d236bd2ef61512e41aa048d7ea3
mouli911 <mounichandu119@gmail.com>
2025-02-19 12:41:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,13 @@ resource "aws_launch_template" "web_server_as" {
     tags = {
         Name = "DevOps"
     }
-    
 }
    
-
-
-  resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-06c6f2353ff34d727", "subnet-0152dcda24decd588"]
-     listener {
+resource "aws_elb" "web_server_lb" {
+    name = "web-server-lb"
+    security_groups = [aws_security_group.web_server.id]
+    subnets = ["subnet-06c6f2353ff34d727", "subnet-0152dcda24decd588"]
+    listener {
       instance_port     = 8000
       instance_protocol = "http"
       lb_port           = 80
@@ -25,7 +22,8 @@ resource "aws_launch_template" "web_server_as" {
     tags = {
       Name = "terraform-elb"
     }
-  }
+}
+
 resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
     min_size             = 1
@@ -33,12 +31,11 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-2b"] 
+    availability_zones   = ["us-east-1a", "us-east-1b"]
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
-      }
-    
-    
-  }
+    }
+}
+
 
